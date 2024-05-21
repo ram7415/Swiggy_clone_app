175b7308cee103fae5374e860630cf41eb8b5e38
ram7415 <yram9757@gmail.com>
2024-05-21 22:55:04 +0530

    intalled tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,39 +6,39 @@ const Header = () => {
   const [loginBtn, setLoginBtn] = useState("Login");
   const onlineStatus = useOnlineStatus();
   return (
-    <div className="header">
+    <div className="flex justify-between shadow-lg ">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} alt="logo" />
-        <div className="nav-items">
-          <ul>
-            <li>OnlineStatus{onlineStatus ? "🟢" : "🔴"}</li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/About">About Us</Link>
-            </li>
-            <li>
-              <Link to="/Contact">Contact Us</Link>
-            </li>
-            <li>
-              <Link to="/Grocery">Grocery</Link>
-            </li>
-            <li>
-              <Link to="/">Cart</Link>
-            </li>
-            <button
-              className="loginBtn"
-              onClick={() => {
-                loginBtn === "Login"
-                  ? setLoginBtn("Logout")
-                  : setLoginBtn("Login");
-              }}
-            >
-              {loginBtn}
-            </button>
-          </ul>
-        </div>
+        <img className="w-56" src={LOGO_URL} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul className="flex p-4 m-4">
+          <li>OnlineStatus{onlineStatus ? "🟢" : "🔴"}</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About Us</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/Grocery">Grocery</Link>
+          </li>
+          <li>
+            <Link to="/">Cart</Link>
+          </li>
+          <button
+            className="loginBtn"
+            onClick={() => {
+              loginBtn === "Login"
+                ? setLoginBtn("Logout")
+                : setLoginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
+        </ul>
       </div>
     </div>
   );
