3197df9cca5d6d0f51158703f39a17700e64bf51
ram7415 <yram9757@gmail.com>
2024-06-02 11:45:37 +0530

    redux added into the project

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -7,16 +7,22 @@ const cartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+      // Redux Toolkit uses immer BTS
       state.items.push(action.payload);
     },
     removeItem: (state, action) => {
       state.items.pop();
     },
+    //originalState = {items: ["pizza"]}
     clearCart: (state, action) => {
-      state.items.length = [];
+      //RTK - either Mutate the existing  state or return a new State
+      // state.items.length = 0; // originalState = []
+
+      return { items: [] }; // this new object will be replaced inside originalState = { items: [] }
     },
   },
 });
 
 export const { addItem, removeItem, clearCart } = cartSlice.actions;
+
 export default cartSlice.reducer;
